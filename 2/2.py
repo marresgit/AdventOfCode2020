b8bd0505c447421f667e7c859131333f07e854ab
Mauricio Saez <saeztoro.mauricio@gmail.com>
2020-12-11 16:00:27 +0100

    Just the if statement left

---
+++ 2/2.py
@@ -0,0 +1,21 @@
+
+input = []
+
+f = open('input.txt', 'r')
+
+for i in f.read().splitlines():
+    input.append(i)
+
+
+for i in input:
+    list_input = i.split()
+    #----------FIX FIRST INPUT [X]----------
+    numbers = list_input[0].split("-")
+    fnum = (int(numbers[0]))
+    lnum = (int(numbers[1])+1)
+    #print(range(fnum,lnum))
+    #----------FIX MIDDLE INPUT [X]----------
+    letter = list_input[1].replace(':', '')
+    #----------FIX LAST INPUT [X]----------
+    #print(list_input[2])
+    #if statement [-]
